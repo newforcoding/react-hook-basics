56a7bd5d15f2c7b867caa19258931e0366d2e2af
pratiksha kale <pratikshakale909@gmail.com>
2021-05-05 23:40:11 +0530

    nothing to be change

--- src/CounterClass.js
+++ src/CounterClass.js
@@ -4,18 +4,22 @@ export default class CounterClass extends Component {
     constructor(){
         super()
         this.state={
-            count:0
+            count:0,
+            time:new Date()
         }
     }
     hanldeCounter=()=>{
         this.setState(prevState=>({count:prevState.count+1}))
+        // this.setState(new Date())
     }
     render() {
         const {count} = this.state
+        const {time} = this.state
         return (
             <div>
                 <h1>Welcome to Class Counter </h1>
-                <button onClick={this.hanldeCounter}>{count}</button>
+                <button onClick={this.hanldeCounter}>{count} </button>
+                <p>at: {`${time.getHours()} : ${time.getMinutes()} : ${time.getSeconds()}`}</p>
             </div>
         )
     }
