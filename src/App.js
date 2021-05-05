d6ddacaadcc91a6a5747cc8850b6c2ccd290a97e
pratiksha kale <pratikshakale909@gmail.com>
2021-05-05 23:05:14 +0530

    Created Class componentd for counter incremenet

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import logo from './logo.svg';
+import React from 'react'
 import './App.css';
+import CounterClass from './CounterClass';
 
 function App() {
   return (
     <div className="App">
-       
-      
+      <CounterClass/> 
     </div>
   );
 }
