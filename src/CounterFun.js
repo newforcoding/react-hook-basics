6312e6db9f008df8545673b30469d1d6da7f97ca
pratiksha kale <pratikshakale909@gmail.com>
2021-05-05 23:37:19 +0530

    add time for every counter increment

--- src/CounterFun.js
+++ src/CounterFun.js
@@ -2,13 +2,16 @@ import React, { useState } from 'react'
 
 function CounterFun() {
     const [count,setCount] = useState(0)
+    const [time,setTime] = useState(new Date())
      const handleCounter =() =>{
             setCount(count+1)
+            setTime(new Date())
     }
     return (
         <div>
             <h1>Welcome to Counter Function</h1>
-            <button onClick={handleCounter}>{count}</button> 
+            <button onClick={handleCounter}>{count} </button>
+            <p>at {`${time.getHours()} : ${time.getMinutes()} : ${time.getSeconds()}`} </p>
         </div>
     )
 }
