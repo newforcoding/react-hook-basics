fb2d1dd96b0f338f28e8f17fb8e9ff2f31635f24
pratiksha kale <pratikshakale909@gmail.com>
2021-05-05 23:10:07 +0530

    Export both the components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import './App.css';
 import CounterClass from './CounterClass';
+import CounterFun from './CounterFun';
 
 function App() {
   return (
     <div className="App">
       <CounterClass/> 
+      <CounterFun/>
     </div>
   );
 }
