2474363431f59e939632c5d71f183f3ae2a60ce3
pratiksha kale <pratikshakale909@gmail.com>
2021-05-06 22:31:11 +0530

    export component

--- src/App.js
+++ src/App.js
@@ -3,13 +3,19 @@ import './App.css';
 import CounterClass from './CounterClass';
 import CounterFun from './CounterFun';
 import FunCounterIcon from './FunCounterIcon';
+import FunEffects from './FunEffects';
+import FunRefs from './FunRefs';
+import HookMemo from './HookMemo';
 
 function App() {
   return (
     <div className="App">
-      <CounterClass/> 
+      {/* <CounterClass/> 
       <CounterFun/>
       <FunCounterIcon/>
+      <FunEffects/>
+      <FunRefs/> */}
+      <HookMemo/>
     </div>
   );
 }
