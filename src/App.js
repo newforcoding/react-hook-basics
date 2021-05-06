5b6b7c96aa9c6adecc153cba70a6394e14938fab
pratiksha kale <pratikshakale909@gmail.com>
2021-05-06 16:47:26 +0530

    comment link and export function

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react'
 import './App.css';
 import CounterClass from './CounterClass';
 import CounterFun from './CounterFun';
+import FunCounterIcon from './FunCounterIcon';
 
 function App() {
   return (
     <div className="App">
       <CounterClass/> 
       <CounterFun/>
+      <FunCounterIcon/>
     </div>
   );
 }
